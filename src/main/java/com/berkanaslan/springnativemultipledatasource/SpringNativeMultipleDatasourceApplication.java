8bd9c8d81d2a073105f07e9b1683dd1dad224613
Berkan Aslan <aslnberkan@gmail.com>
2023-06-17 18:03:53 +0300

    Project initialized.

---
+++ src/main/java/com/berkanaslan/springnativemultipledatasource/SpringNativeMultipleDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.berkanaslan.springnativemultipledatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeMultipleDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNativeMultipleDatasourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berkanaslan/springnativemultipledatasource/SpringNativeMultipleDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkanaslan.springnativemultipledatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeMultipleDatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
