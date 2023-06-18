fe0c143b743cc8989729dd002ea8ebf4e79a5fbf
Berkan Aslan <aslnberkan@gmail.com>
2023-06-18 21:20:54 +0300

    First native image built. It took 4 mins.

--- src/main/java/com/berkanaslan/springnativemultipledatasource/controller/PersonController.java
+++ src/main/java/com/berkanaslan/springnativemultipledatasource/controller/PersonController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping(PersonController.PATH)
 @RequiredArgsConstructor
@@ -19,7 +17,7 @@ public class PersonController {
     private final PersonRepository personRepository;
 
     @GetMapping(path = "{id}")
-    public Person findById(@PathVariable(name = "id") UUID id) {
+    public Person findById(@PathVariable(name = "id") long id) {
         return personRepository.findById(id).orElse(null);
     }
 }
--- src/main/java/com/berkanaslan/springnativemultipledatasource/repository/PersonRepository.java
+++ src/main/java/com/berkanaslan/springnativemultipledatasource/repository/PersonRepository.java
@@ -4,8 +4,6 @@ import com.berkanaslan.springnativemultipledatasource.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface PersonRepository extends JpaRepository<Person, UUID> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
